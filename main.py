e1b5a0dfc7ceb8abf2723515b400aa90b4f20c33
perryvw12 <perry.vw12@gmail.com>
2024-10-08 09:22:17 -0600

    Created files for helper function to parse the text and main file.

---
+++ main.py
@@ -0,0 +1,6 @@
+import sys
+
+with open(r"C:\Users\perry\OneDrive\Desktop\Classes\projects\LibertyJailLetters_Analysis\Letter1.txt") as text1:
+    with open(r"C:\Users\perry\OneDrive\Desktop\Classes\projects\LibertyJailLetters_Analysis\Letter2.txt") as text2:
+        print()
+
