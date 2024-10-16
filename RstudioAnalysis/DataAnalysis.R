51cf48bf2744789f8f3b57c73bca558361679eed
perryvw12 <perry.vw12@gmail.com>
2024-10-16 11:58:04 -0600

    Updated R scripts to remove filler words using the nlk stopword list.

--- RstudioAnalysis/DataAnalysis.R
+++ RstudioAnalysis/DataAnalysis.R
@@ -1,8 +1,12 @@
 library(tidyverse)
+library(stopwords)
+
+'%!in%' <- function(x,y)!('%in%'(x,y))
+wordsToRemove = stopwords(language="en", source="nltk")
 
 letter1 = arrange(letter1, desc(count)) |>
-  mutate(word = str_sub(word, ".", ""))
+  filter(word %!in% wordsToRemove)
 
 letter2 = arrange(letter2, desc(count)) |>
-  mutate(word = str_sub(word, ".", ""))
+  filter(word %!in% wordsToRemove)
 
